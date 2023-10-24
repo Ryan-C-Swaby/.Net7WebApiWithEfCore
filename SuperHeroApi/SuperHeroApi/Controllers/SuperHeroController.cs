9c96aa057787b00c007973b0a1904f473d80c68e
Ryan Swaby <ryan.swaby@gmail.com>
2023-10-23 20:33:12 -0400

    Remove WeatherForecast controller and model Add models project Create SuperHero Model Create SuperHeroApiController Add Crud endpoints to SuperHeroApiController

---
+++ SuperHeroApi/SuperHeroApi/Controllers/SuperHeroController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperHeroApi.Models;
+
+namespace SuperHeroApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private static List<SuperHero> _superHeroes = new List<SuperHero>()
+        {
+            new SuperHero()
+            {
+                Id = 1,
+                Name = $"Batman",
+                FirstName = "Bruce",
+                LastName = "Wayne",
+                Place = "Gotham"
+            },
+            new SuperHero()
+            {
+                Id = 2,
+                Name = "Superman",
+                FirstName = "Clark",
+                LastName = "Kent",
+                Place = "Metropolis"
+            }
+        };
+
+
+        [HttpGet]
+        [Route("getall")]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(_superHeroes);
+        }
+
+        [HttpGet]
+        [Route("get{id:int}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var superHero = _superHeroes.Where(x => x.Id == id).FirstOrDefault();
+
+            if (superHero == null)
+            {
+                return NotFound($"No superherod found with id: {id}");
+            }
+
+            return Ok(superHero);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(SuperHero newSuperHero)
+        {
+            int? maxSuperHeroId = _superHeroes.Max(x => x.Id) + 1;
+
+            newSuperHero.Id = maxSuperHeroId;
+
+            _superHeroes.Add(newSuperHero);
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var superHero = _superHeroes.Where(x => x.Id!= id).FirstOrDefault();
+
+            if (superHero == null)
+            {
+                return BadRequest($"No super hero found with id: {id}");
+            }
+
+            _superHeroes.Remove(superHero);
+
+            return Ok();
+        }
+    }
+}
